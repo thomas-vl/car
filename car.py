a24f6be4fcc10622869de3b061c4af1dbdb7ce61
thomas-vl <thomas.v.latum@gmail.com>
2017-03-27 21:31:12 +0200

    Removed obsolete code

--- car.py
+++ car.py
@@ -43,7 +43,7 @@ class light(object):
 
     def blink(self,times):
         self.times = times
-        t = threading.Thread(target=self.blinkWorker)
+        t = threading.Thread(target=self.blinkThread)
         threads.append(t)
         t.start()
 
@@ -57,39 +57,6 @@ except:
 lightFL = light(21)
 lightFR = light(16)
 lightFL.blink(5)
-
-lights = {
-    "LeftFront":{"pin":21,"status":0},
-    "RightFront":{"pin":16,"status":0}
-    }
-
-def lightCtrl(names,status):
-    for i in names:
-        io.digitalWrite(lights[i]["pin"],status)
-        lights[i]["status"] = status
-
-def initResource():
-    try:
-        io.wiringPiSetupGpio()
-    except:
-        print "GPIO issue", sys.exc_info()[0]
-    for key, value in lights.items():
-        #make pins into output
-        io.pinMode(value["pin"],1)
-        #set output low
-        io.digitalWrite(value["pin"],0)
-
-def lightTest():
-    lightCtrl(["LeftFront"],1)
-    time.sleep(3)
-    lightCtrl(["LeftFront"],0)
-    lightCtrl(["RightFront"],1)
-    time.sleep(3)
-    lightCtrl(["RightFront"],0)
-    time.sleep(1)
-    lightCtrl(["LeftFront","RightFront"],1)
-    time.sleep(3)
-    lightCtrl(["LeftFront","RightFront"],0)
-
-#initResource()
-#lightTest()
+lightFR.on()
+time.sleep(3)
+LightFR.off()
