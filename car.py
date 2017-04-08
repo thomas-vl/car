bffcd1d2b648901968f894f07099875c512684c8
thomas-vl <thomas.v.latum@gmail.com>
2017-04-08 19:50:09 +0200

    Removed car class

--- car.py
+++ car.py
@@ -2,104 +2,97 @@ import time, os, sys, threading, picamera
 import wiringpi
 
 
-class car(object):
+
+class cameraClass(object):
     def __init__(self):
-        #initialise GPIO
-        try:
-            wiringpi.wiringPiSetupGpio()
-        except:
-            print ("GPIO issue", sys.exc_info()[0])
+        #initialise camera
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (1024, 768)
+        self.camera.start_preview()
 
-    class camera(object):
-        def __init__(self):
-            #initialise camera
-            self.camera = picamera.PiCamera()
-            self.camera.resolution = (1024, 768)
-            self.camera.start_preview()
+    def picture(self):
+        self.camera.capture('image.jpg')
 
-        def picture(self):
-            self.camera.capture('image.jpg')
+class crashSensor(object):
+    def __init__(self):
+        self.pin = 16
+        self.alive = True
+        wiringpi.pinMode(self.pin,0)
+        wiringpi.pullUpDnControl(self.pin,2)
 
-    class crashSensor(object):
-        def __init__(self):
-            self.pin = 16
-            self.alive = True
-            wiringpi.pinMode(self.pin,0)
-            wiringpi.pullUpDnControl(self.pin,2)
+    def crashWorker(self):
+        while self.alive:
+            state = wiringpi.digitalRead(self.pin)
+            if state == 0:
+                self.crash()
+                self.alive = False
 
-        def crashWorker(self):
-            while self.alive:
-                state = wiringpi.digitalRead(self.pin)
-                if state == 0:
-                    self.crash()
-                    self.alive = False
+    def check(self):
+        t = threading.Thread(name='crash',target=self.crashWorker)
+        t.start()
 
-        def check(self):
-            t = threading.Thread(name='crash',target=self.crashWorker)
-            t.start()
-
-        def crash(self):
-            c.lightFL.blink(2)
-            c.lightFR.blink(2)
+    def crash(self):
+        c.lightFL.blink(2)
+        c.lightFR.blink(2)
 
 
-    class light(object):
-        def __init__(self, pin):
-            #make pins into output
-            wiringpi.pinMode(pin,1)
-            #set output low
-            wiringpi.digitalWrite(pin,0)
-            #set variables
-            self.status = 0
-            self.pin = pin
+class light(object):
+    def __init__(self, pin):
+        #make pins into output
+        wiringpi.pinMode(pin,1)
+        #set output low
+        wiringpi.digitalWrite(pin,0)
+        #set variables
+        self.status = 0
+        self.pin = pin
 
-        def on(self):
-            #turn light on
-            wiringpi.digitalWrite(self.pin,1)
-            self.status = 1
+    def on(self):
+        #turn light on
+        wiringpi.digitalWrite(self.pin,1)
+        self.status = 1
 
-        def off(self):
-            #turn light off
-            wiringpi.digitalWrite(self.pin,0)
-            self.status = 0
+    def off(self):
+        #turn light off
+        wiringpi.digitalWrite(self.pin,0)
+        self.status = 0
 
-        def blinkWorker(self,pause):
-            #frits: ik ben er vrij zeker van dat dit met minder code kan.
-            if (self.status == 0):
-                self.on()
-                time.sleep(pause)
-                self.off()
-                time.sleep(pause)
-            else:
-                self.off()
-                time.sleep(pause)
-                self.on()
-                time.sleep(pause)
+    def blinkWorker(self,pause):
+        #frits: ik ben er vrij zeker van dat dit met minder code kan.
+        if (self.status == 0):
+            self.on()
+            time.sleep(pause)
+            self.off()
+            time.sleep(pause)
+        else:
+            self.off()
+            time.sleep(pause)
+            self.on()
+            time.sleep(pause)
 
-        def blinkThread(self):
-            for _ in range(self.times):
-                self.blinkWorker(0.5)
+    def blinkThread(self):
+        for _ in range(self.times):
+            self.blinkWorker(0.5)
 
-        def blink(self,times):
-            self.times = times
-            #Dit is een example voor threading omdat ik deze functie async wil laten lopen misschien moet het wat schoon gemaakt worden
-            #meer info over threading: https://pymotw.com/2/threading/
-            t = threading.Thread(name='light',target=self.blinkThread)
-            t.start()
+    def blink(self,times):
+        self.times = times
+        #Dit is een example voor threading omdat ik deze functie async wil laten lopen misschien moet het wat schoon gemaakt worden
+        #meer info over threading: https://pymotw.com/2/threading/
+        t = threading.Thread(name='light',target=self.blinkThread)
+        t.start()
 
 
 def cameraTest():
-    c.cam.picture()
+    camera.picture()
 
 def lightTest():
-    c.lightFL.on()
-    c.lightFR.on()
+    lightFL.on()
+    lightFR.on()
     time.sleep(2)
-    c.lightFL.off()
-    c.lightFR.off()
+    lightFL.off()
+    lightFR.off()
 
 def crashTest():
-    c.crashS.check()
+    crashS.check()
 
 def motorTest():
    wiringpi.pinMode(4,OUTPUT)
@@ -109,7 +102,7 @@ def motorTest():
    print("spinning!\n")
    wiringpi.pwmWrite(18,50)
    wiringpi.digitalWrite(4,HIGH)
-   wiringpi.digitalWrite(17,LOW)
+   wiringpi.digitalWrite(17,HIGH)
 
    time.sleep(3)
    print("stopping\n")
@@ -118,11 +111,10 @@ def motorTest():
 
 
 
-c = car()
-c.lightFL = c.light(26)
-c.lightFR = c.light(20)
-c.crashS = c.crashSensor()
-c.cam = c.camera()
+lightFL = light(26)
+lightFR = light(20)
+crashS = crashSensor()
+camera = cameraClass()
 
 test = input('What do you want to test:(light,camera,crash)')
 if (test == "light"):
