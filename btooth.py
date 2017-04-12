ba71a969883342578f9134fce161e3a22721da5c
thomas-vl <thomas.v.latum@gmail.com>
2017-04-12 08:35:05 +0200

    btooth2

--- btooth.py
+++ btooth.py
@@ -1,7 +1,48 @@
-import bluetooth
+import bluetooth import *
+import sys
 
 nearby_devices = bluetooth.discover_devices(lookup_names=True)
 print("found %d devices" % len(nearby_devices))
 
 for addr, name in nearby_devices:
     print("  %s - %s" % (addr, name))
+
+
+if sys.version < '3':
+    input = raw_input
+
+addr = "9C:65:B0:78:51:C4"
+
+if len(sys.argv) < 2:
+    print("no device specified.  Searching all nearby bluetooth devices for")
+    print("the SampleServer service")
+else:
+    addr = sys.argv[1]
+    print("Searching for SampleServer on %s" % addr)
+
+# search for the SampleServer service
+uuid = "00001101-0000-1000-8000-00805F9B34FB"
+service_matches = find_service( uuid = uuid, address = addr )
+
+if len(service_matches) == 0:
+    print("couldn't find the SampleServer service =(")
+    sys.exit(0)
+
+first_match = service_matches[0]
+port = first_match["port"]
+name = first_match["name"]
+host = first_match["host"]
+
+print("connecting to \"%s\" on %s" % (name, host))
+
+# Create the client socket
+sock=BluetoothSocket( RFCOMM )
+sock.connect((host, port))
+
+print("connected.  type stuff")
+while True:
+    data = input()
+    if len(data) == 0: break
+    sock.send(data)
+
+sock.close()
