7702c7c817187a1b4219e7e4f2c6c5a62038b0ea
thomas-vl <thomas.v.latum@gmail.com>
2017-04-12 14:09:03 +0200

    .

--- car.py
+++ car.py
@@ -1,3 +1,4 @@
+from bluetooth import *
 import time, os, sys, threading, picamera
 import wiringpi
 
@@ -157,6 +158,47 @@ def motorTest():
     time.sleep(1)
     motor.stop()
 
+def btTest():
+    if sys.version < '3':
+        input = raw_input
+
+    addr = "9C:65:B0:78:51:C4"
+
+    if len(sys.argv) < 2:
+        print("no device specified.  Searching all nearby bluetooth devices for")
+        print("the SampleServer service")
+    else:
+        addr = sys.argv[1]
+        print("Searching for SampleServer on %s" % addr)
+
+    # search for the SampleServer service
+    uuid = "00001101-0000-1000-8000-00805F9B34FB"
+    service_matches = find_service( uuid = uuid, address = addr )
+
+    if len(service_matches) == 0:
+        print("couldn't find the SampleServer service =(")
+        sys.exit(0)
+
+    first_match = service_matches[0]
+    port = first_match["port"]
+    name = first_match["name"]
+    host = first_match["host"]
+
+    print("connecting to \"%s\" on %s" % (name, host))
+
+    # Create the client socket
+    sock=BluetoothSocket( RFCOMM )
+    sock.connect((host, port))
+
+    print("connected.")
+    while True:
+        data = sock.recv(1024)
+        if len(data) > 0:
+            print(data)
+        if data == "b'forward'":
+            motor.forward()
+        if data == "b'idle'":
+            motor.stop()
 
 try:
     wiringpi.wiringPiSetupGpio()
@@ -179,5 +221,5 @@ if (test == "crash"):
     crashTest()
 if (test == "motor"):
     motorTest()
-if (test == "steer"):
-    steerTest()
+if (test == "bt"):
+    btTest()
